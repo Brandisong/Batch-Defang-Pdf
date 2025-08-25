a256dad36d8d273b6e569f4c175238629daeca21
Brandisong <brandisong00@gmail.com>
2025-08-25 14:45:40 +1000

    Fixed up deconstructing pdfs for now. The arguments in a list had to be separate list items. ARGS_FOR_PDFTOCAIRO no longer works at the moment, but can be customised at the bottom of the 'deconstruct pdfs' section. Next up is assembling pdfs using merge2pdf.

--- batch_defang_pdf.py
+++ batch_defang_pdf.py
@@ -20,9 +20,9 @@ class PdfFile:
 
 # Check if directory argument is supplied
 try:
-    root_directory = Path(argv[1])
+    root_directory = Path(argv[-1])
 except IndexError:
-    print("Usage: py scrape_pdfs.py <directory of pdfs>")
+    print("Usage: py batch_defang_pdf.py [options] <directory of pdfs>")
     exit()
 
 # Check if directory is valid
@@ -36,6 +36,22 @@ if "-v" in argv or "--verbose" in argv:
 else:
     verbose = False
 
+if "-d" in argv or "--deconstruct" in argv:
+    deconstruct = True
+else:
+    deconstruct = False
+
+if "-a" in argv or "--assemble" in argv:
+    assemble = True
+else:
+    assemble = False
+
+if deconstruct == False and assemble == False:
+    print("Error: Please specifiy whether you want to:")
+    print(" Deconstruct pdfs [-d | --deconstruct]")
+    print(" Assemble pdfs [-a | --assemble]")
+    exit()
+
 # Make a scrubbed directory if needed
 root_directory_scrubbed = Path(str(root_directory) + "_scrubbed")
 if not root_directory_scrubbed.exists():
@@ -44,28 +60,38 @@ if not root_directory_scrubbed.exists():
 
 # MAIN PROCESS STARTS HERE
 
-# Get list of all pdfs
-pdf_directory_list = sorted(root_directory.glob(r'**/*.pdf'))
-if verbose:
-    print("-- FOUND PDF FILES --", )
-    for pdf in pdf_directory_list:
-        print(pdf)
-    print("Processing...")
-
-# Convert to objects and put them in a list
-pdf_list = []
-for pdf_directory in pdf_directory_list:
-    pdf_list.append(PdfFile(pdf_directory))
-
-
-# Iterate over each pdf
-for pdf in pdf_list:
-    # Make a folder for each file
-    if not pdf.directory_scrubbed.exists():
-        pdf.directory_scrubbed.mkdir(parents=True)
-
-    # Call pdftocairo on each file
-    # Usage: pdftocairo [options] <PDF-file> [<output-file>]
-    run(["pdftocairo", ARGS_FOR_PDFTOCAIRO, pdf.directory, pdf.output_file])
+# DECONSTRUCT PDFS
+if deconstruct:
+    # Get list of all pdfs
+    pdf_directory_list = sorted(root_directory.glob(r'**/*.pdf'))
     if verbose:
-        print(f"Successfully processed: {pdf.directory_scrubbed}")
+        print("-- FOUND PDF FILES --", )
+        for pdf in pdf_directory_list:
+            print(pdf)
+        print("Processing...")
+
+    # Convert to objects and put them in a list
+    pdf_list = []
+    for pdf_directory in pdf_directory_list:
+        pdf_list.append(PdfFile(pdf_directory))
+
+    # Iterate over each pdf
+    for pdf in pdf_list:
+        # Make a folder for each file
+        if not pdf.directory_scrubbed.exists():
+            pdf.directory_scrubbed.mkdir(parents=True)
+
+        # Call pdftocairo on each file
+        # Usage: pdftocairo [options] <PDF-file> [<output-file>]
+        run(["pdftocairo", "-png", "-r", "300", pdf.directory, pdf.output_file]) # ARGS GO HERE
+        if verbose:
+            print(f"Successfully processed: {pdf.directory_scrubbed}")
+    
+    print("-- Processing complete --")
+
+# ASSEMBLE PDFS
+# (not yet implemented)
+
+# Below is the merge2pdf documentation for merging from a directory:
+#   Merge all Images/PDFs of one or multiple directory
+#   merge2pdf output.pdf path/to/a/dir path/to/another ...
